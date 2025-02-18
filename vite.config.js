5b7b7eed6079ca1d1d3e219b538f48d373501801
Jashanpreet Singh <jashanpreetsingh1096@gmail.com>
2025-02-18 13:50:57 +0530

    vite build changed

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwind from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     cors: {
       origin: "*",
-    },    
+    },
     proxy: {
-      '/api': {
-        target: 'http://3.82.158.190:8000',
+      "/api": {
+        target: "http://3.82.158.190:8000",
         changeOrigin: true,
         secure: false,
       },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
     },
   },
 });
